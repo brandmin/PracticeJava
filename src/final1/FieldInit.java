2cb5c40cdcc53bc1d13e9a33e0c9617f8c7607e3
brandmin <overness@naver.com>
2024-01-25 14:46:50 +0900

    240125 Final

---
+++ src/final1/FieldInit.java
@@ -0,0 +1,10 @@
+package final1;
+
+public class FieldInit {
+    static final int CONST_VALUE = 10; // static final를 사용하면 변수명을 대문자로 쓰기
+    final int value = 10;
+
+//    public FieldInit(int value) {
+//        this.value = value; // 이미 초기에 할당되었다면 생성자에 추가할 수 없다.
+//    }
+}
